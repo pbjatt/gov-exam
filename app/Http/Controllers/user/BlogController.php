8adb76aa86f3143268d4522934e7890a18400d15
shubham-cstechguru <shubham.cstechguru@gmail.com>
2021-04-05 20:20:40 +0530

    gov is going

--- app/Http/Controllers/user/BlogController.php
+++ app/Http/Controllers/user/BlogController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\BlogRequest;
 use App\Model\Blog;
 use App\Model\Exam_category;
 use App\Model\Setting;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Auth;
+use Intervention\Image\ImageManagerStatic as Image;
 
 class BlogController extends Controller
 {
@@ -71,9 +74,34 @@ class BlogController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(BlogRequest $request)
     {
-        //
+        $blog = new Blog();
+
+        if ($request->hasFile('blog_image')) {
+
+            $image       = $request->file('blog_image');
+            $filename    = $image->getClientOriginalName();
+
+            $image_resize = Image::make($image->getRealPath());
+            $image_resize->resize(500, 500, function ($constraint) {
+                $constraint->aspectRatio();
+            })->save(public_path('storage/blog/' . $filename));
+
+            $blog->blog_image = $filename;
+        }
+
+        $userId = Auth::guard()->user()->id;
+        $blog->blog_title = $request->blog_title;
+        $blog->blog_slug = Str::slug($request->blog_title.''.$userId, '-');
+        $blog->blog_desc = $request->blog_desc;
+        $blog->category_id = $request->category_id;
+        $blog->blog_seotitle = $request->blog_seotitle;
+        $blog->blog_seodescription = $request->blog_seodescription;
+        $blog->blog_seokeyword = $request->blog_seokeyword;
+        $blog->save();
+
+        return redirect(route('admin.blog.index'))->with('success', 'Blog successfully added.');
     }
 
     /**
