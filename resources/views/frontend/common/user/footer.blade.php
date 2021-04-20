476b8b79f0e51cd5a62a32b26a16b94d87cd3df8
shubham-cstechguru <shubham.cstechguru@gmail.com>
2021-04-20 18:19:46 +0530

    editor add

--- resources/views/frontend/common/user/footer.blade.php
+++ resources/views/frontend/common/user/footer.blade.php
@@ -7,16 +7,56 @@
 {{ HTML::script('assets/js/admin.js') }}
 {{ HTML::script('assets/js/pages/dashboard/dashboard3.js') }}
 {{ HTML::script('assets/js/pages/forms/editors.js') }}
-{{ HTML::script('assets/js/bundles/tinymce/tinymce.min.js') }}
+{{ HTML::script('https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js') }}
 {{ HTML::script('js/main.js') }}
 
 <script>
-    $('.datepicker').datepicker({
-        autoclose: true,
-        format: 'yyyy-mm-dd',
-    });
+    tinymce.init({
+        selector: '.editor',
+        // plugins: 'image code',
+        plugins: "code, link, image code, textcolor, emoticons, hr, lists, charmap, table, fullscreen",
+        fontsizeselect: true,
+        // toolbar: 'undo redo | link image | code',
+        toolbar: 'bold italic underline strikethrough link image forecolor backcolor | formatselect h1 h2 h3 h4 | table hr superscript subscript | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | uploadImageButton | fullscreen | code',
+        //     
+        /* enable title field in the Image dialog*/
+        image_title: true,
+        /* enable automatic uploads of images represented by blob or data URIs*/
+        automatic_uploads: true,
+        /*
+          URL of our upload handler (for more details check: https://www.tiny.cloud/docs/configure/file-image-upload/#images_upload_url)
+          images_upload_url: 'postAcceptor.php',
+          here we add custom filepicker only to Image dialog
+        */
+        file_picker_types: 'image',
+        /* and here's our custom image picker*/
+        file_picker_callback: function(cb, value, meta) {
+            var input = document.createElement('input');
+            input.setAttribute('type', 'file');
+            input.setAttribute('accept', 'image/*');
 
-    var useDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+            input.onchange = function() {
+                var file = this.files[0];
+
+                var reader = new FileReader();
+                reader.onload = function() {
+                    var id = 'blobid' + (new Date()).getTime();
+                    var blobCache = tinymce.activeEditor.editorUpload.blobCache;
+                    var base64 = reader.result.split(',')[1];
+                    var blobInfo = blobCache.create(id, file, base64);
+                    blobCache.add(blobInfo);
+
+                    /* call the callback and populate the Title field with the file name */
+                    cb(blobInfo.blobUri(), {
+                        title: file.name
+                    });
+                };
+                reader.readAsDataURL(file);
+            };
+
+            input.click();
+        },
+    });
 </script>
 </body>
 
