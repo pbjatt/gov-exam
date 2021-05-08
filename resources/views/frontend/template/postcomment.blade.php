d4bf4f49700206c2bb3c6079afecf70386b2ac88
Shubham Jajoo <shubham.cstechguru@gmail.com>
2021-05-08 14:55:27 +0530

    reply design

--- resources/views/frontend/template/postcomment.blade.php
+++ resources/views/frontend/template/postcomment.blade.php
@@ -36,6 +36,24 @@
                 <div class="mt-3">
                     {{ $comment->message }}
                 </div>
+
+                <div class="replyinput">
+                    <div class="mt-4 d-flex justify-content-between replymsg">
+                        <div class="w-100 mr-4">
+                            <input class="blogmessage" name="blogmessage" class="squareInput" required placeholder="reply to {{ $comment->user->name }}" type="text" value="" style="border: 1px solid; border-radius: 16px; border-color: #00000078; padding: 8px 8px; height: auto; font-size: 12px;">
+                        </div>
+                        <div class="comment-button">
+                            @if(Auth::user())
+                            <div class="addblogcomment comment-btn" data-url="{{ url('ajex/blogcomment') }}" data-type="{{ $blog->post_type }}" data-comment="{{ $comment->id }}" data-blog="{{ $blog->id }}"><i class="fas fa-reply p-0 icon-color"></i></div>
+                            @endif
+                            @if(!Auth::user())
+                            <a href="{{ url('account/login') }}">
+                                <div class="comment-btn" data-url="{{ url('ajex/blogcomment') }}" data-blog="{{ $blog->id }}"><i class="fas fa-reply p-0 icon-color"></i></div>
+                            </a>
+                            @endif
+                        </div>
+                    </div>
+                </div>
                 @foreach($comment->replay_comments as $reply)
                 <div class="media mt-4" style="border-left: 1px dashed;">
                     <img class="ml-1" src="{{ url('extraimage/images.jpg') }}" width="36" height="36" alt="User">
@@ -58,23 +76,7 @@
                 </div>
                 @endforeach
 
-                <div class="replyinput">
-                    <div class="mt-4 d-flex justify-content-between replymsg">
-                        <div class="w-100 mr-4">
-                            <input class="blogmessage" name="blogmessage" class="squareInput" required placeholder="reply to {{ $comment->user->name }}" type="text" value="" style="border: 1px solid; border-radius: 16px; border-color: #00000078; padding: 8px 8px; height: auto; font-size: 12px;">
-                        </div>
-                        <div class="comment-button">
-                            @if(Auth::user())
-                            <div class="addblogcomment comment-btn" data-url="{{ url('ajex/blogcomment') }}" data-type="{{ $blog->post_type }}" data-comment="{{ $comment->id }}" data-blog="{{ $blog->id }}"><i class="fas fa-reply p-0 icon-color"></i></div>
-                            @endif
-                            @if(!Auth::user())
-                            <a href="{{ url('account/login') }}">
-                                <div class="comment-btn" data-url="{{ url('ajex/blogcomment') }}" data-blog="{{ $blog->id }}"><i class="fas fa-reply p-0 icon-color"></i></div>
-                            </a>
-                            @endif
-                        </div>
-                    </div>
-                </div>
+
             </div>
             <div class="comment-reply text-right replybtn">
                 <a href="javascript:void(0)">
